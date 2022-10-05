ebc82be81e88d6b5612973265709cbf24b11861a
developer <crocodile305@yandex.com>
2022-10-05 10:58:59 +0800

    added func

--- index.php
+++ index.php
@@ -1,42 +1,32 @@
 <?php
-testimg();
-function mainfunc($response){
-    $res = json_decode($response, TRUE);
-    $img_url = isset($res) && isset($res['img_src']) ? $res['img_src'] : null;
-    $my_img_link = "";
-    if($img_url !== null){
-      $my_img_name = getimagename($img_url);
-      $my_img_link = downloadimagefromurl($img_url,$my_img_name);
+
+$img_url = isset($_GET['url']) ? $_GET['url'] : null;
+
+if($img_url !== null){
+    $my_img_name = getimagename($img_url);
+    $my_img_link = downloadimagefromurl($img_url,$my_img_name);
+    echo $my_img_link;
+    
+    function downloadimagefromurl($url="",$name=""){
+        $main_domain_url = "https://img-viewer-3325.herokuapp.com/";
+        $my_img_url = "";
+        if($url !=="" && isset($url) && $name !=="" && isset($name)){
+          $content = file_get_contents($url);
+          file_put_contents($name, $content);
+        }
+        $my_img_url = $main_domain_url.$name;
+        return $my_img_url;
     }
-    $res["img_src"] = $my_img_link;
-    return json_encode($res);
-  }
-  
-  function getimagename($url=''){
-    $params = explode($url);
-    if(count($params) >0){
-      return $params[count($params) - 1];
-    }
-    else {
-      return "";
-    }
-  }
-  function downloadimagefromurl($url="",$name=""){
-    $direct = "/";
-    $main_domain_url = "https://****/";
-    $my_img_url = "";
-    if($url !=="" && isset($url) && $name !=="" && isset($name)){
-      $content = file_get_contents($url);
-      file_put_contents($direct.$name, $content);
-    }
-    $my_img_url = $main_domain_url.$direct.$name;
-    return $my_img_url;
-  }
-  
-  function testimg(){
-    $url = "https://www.webuyanycar.com/wbacimages/5071_647_650_648_2003_2006.jpg";
-    $content = file_get_contents($url);
-    file_put_contents("aaa.jpg", $content);
-    echo "ok";
-  }
+    
+    function getimagename($url=''){
+        $params = explode("/",$url);
+        if(count($params) >0){
+          return $params[count($params) - 1];
+        }
+        else {
+          return "";
+        }
+      }
+}
+
 ?>
